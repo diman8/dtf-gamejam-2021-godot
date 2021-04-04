b026c0f9dea799c628f12aa56c7153f197bda13f
Dmitriy Buyakov <dbuyakov@gmail.com>
2021-04-04 12:25:08 +0300

    clean up

--- level/prefabs/cube.gd
+++
@@ -1,7 +0,0 @@
-extends CSGBox
-
-onready var collision_shape : CollisionShape = $StaticBody/CollisionShape
-
-func _ready():
-	collision_shape.shape = BoxShape.new()
-	collision_shape.shape.extents = Vector3(width / 2, height / 2, depth / 2) 
